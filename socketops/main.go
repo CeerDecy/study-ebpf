92cd0133c54bd473515460e1aa74ecf3e4f436a2
CeerDecy <1748788674@qq.com>
2025-01-03 13:46:36 +0800

    socketops

--- socketops/main.go
+++ socketops/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
-	"time"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 // https://eunomia.dev/zh/tutorials/29-sockops/#ebpf_1
@@ -13,44 +17,75 @@ import (
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go bpf_redirect bpf_redirect.bpf.c -- -I../headers
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go bpf_contrack bpf_contrack.bpf.c -- -I../headers
 
+var mount = "/sys/fs/bpf"
+
 func main() {
 
-	mountPath := "/sys/fs/bpf/"
-
-	if err := unix.Mount("bpf", mountPath, "bpf", 0, ""); err != nil {
+	if err := unix.Mount("bpf", mount, "bpf", 0, ""); err != nil {
 		logrus.Fatalln(err)
 	}
+	defer unix.Unmount(mount, 0)
 
-	var contrackObject bpf_contrackObjects
-	if err := loadBpf_contrackObjects(&contrackObject, &ebpf.CollectionOptions{
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatalf("failed to adjust rlimit: %v", err)
+	}
+
+	var contrackObj bpf_contrackObjects
+	var redirect bpf_redirectObjects
+
+	LoadContrackBpf(&contrackObj)
+	LoadRedirectBpf(&redirect, &contrackObj)
+	defer contrackObj.Close()
+	defer redirect.Close()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
+	<-sig
+}
+
+func LoadRedirectBpf(obj *bpf_redirectObjects, b *bpf_contrackObjects) {
+	if err := loadBpf_redirectObjects(obj, &ebpf.CollectionOptions{
 		Maps: ebpf.MapOptions{
-			PinPath:        mountPath,
-			LoadPinOptions: ebpf.LoadPinOptions{},
+			PinPath: mount,
 		},
 	}); err != nil {
-		logrus.Infoln(err)
-		return
+		logrus.Fatalln("load redirect bpf failed", err)
 	}
-	defer contrackObject.Close()
 
-	logrus.Infoln("MapOptions has pinned ====> ", contrackObject.SockOpsMap.IsPinned())
+	if err := link.RawAttachProgram(link.RawAttachProgramOptions{
+		Target:  b.SockOpsMap.FD(),
+		Program: obj.BpfRedir,
+		Attach:  ebpf.AttachSkMsgVerdict,
+	}); err != nil {
+		logrus.Fatalln("attach ebpf program failed", err)
+	}
 
-	if err := contrackObject.SockOpsMap.Pin(mountPath + "sock_ops_map"); err != nil {
+}
+
+func LoadContrackBpf(obj *bpf_contrackObjects) {
+
+	if err := loadBpf_contrackObjects(obj, &ebpf.CollectionOptions{
+		Maps: ebpf.MapOptions{
+			PinPath: mount,
+		},
+	}); err != nil {
+		logrus.Fatalln("load contrack bpf prog error: ", err)
+	}
+
+	logrus.Infoln("MapOptions has pinned ====> ", obj.SockOpsMap.IsPinned())
+
+	if err := obj.SockOpsMap.Pin(mount + "/sock_ops_map"); err != nil {
 		logrus.Fatalf("failed to pin map, error: %v", err)
 	}
 
-	logrus.Infoln("Custom has pinned ====> ", contrackObject.SockOpsMap.IsPinned())
+	logrus.Infoln("Custom has pinned ====> ", obj.SockOpsMap.IsPinned())
 
-	cgroup, err := link.AttachCgroup(link.CgroupOptions{
+	_, err := link.AttachCgroup(link.CgroupOptions{
 		Path:    "/sys/fs/cgroup/",
 		Attach:  ebpf.AttachCGroupSockOps,
-		Program: contrackObject.BpfSockopsHandler,
+		Program: obj.BpfSockopsHandler,
 	})
 	if err != nil {
-		logrus.Infoln(err)
-		return
+		log.Fatalf("failed to attach group contrack eBPF program spec: %v", err)
 	}
-	defer cgroup.Close()
-
-	time.Sleep(time.Hour * 10)
 }
